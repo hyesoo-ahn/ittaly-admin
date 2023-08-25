f268f321e33248c1ae417cbdd5b99b25865f1a23
hyesoo-ahn <ahnhs719@gmail.com>
2023-08-25 17:01:21 +0900

    8/25 fix config

--- src/common/config.tsx
+++ src/common/config.tsx
@@ -1,4 +1,4 @@
-export const URI: string = "http://43.200.65.104";
+export const URI: string = "https://api.ittaly.shop";
 // export const URI: string = "http://10.185.15.21:3000";
 export const FILE_URI: string = "http://43.201.166.188";
 export const ADMIN_TOKEN: string =
